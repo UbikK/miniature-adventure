6ef5145dfabfbd06c2d88febbc789daa88ab26fe
UbikK <cohen.alberic@gmail.com>
2023-01-05 21:55:56 +0100

    login

--- src/helpers/supabase.ts
+++ src/helpers/supabase.ts
@@ -1,10 +1,11 @@
-import {SUPABASE_URL, SUPABASE_ANON_KEY} from '@env';
+import 'react-native-url-polyfill/auto';
+import {SUPABASE_ANON_KEY, SUPABASE_URL} from '@env';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {createClient} from '@supabase/supabase-js';
 
 const supabaseUrl = SUPABASE_URL;
 const supabaseAnonKey = SUPABASE_ANON_KEY;
-
+console.info('supabaseUrl', supabaseUrl);
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage as any,
