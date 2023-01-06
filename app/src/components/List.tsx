1c05201f3e420469b9321d303f4a51efda1cff29
UbikK <cohen.alberic@gmail.com>
2023-01-06 16:07:44 +0100

    monorepo

---
+++ app/src/components/List.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const List = () => {
+  return (
+    <View>
+      <Text style={{color: 'black'}}>This is a list</Text>
+    </View>
+  );
+};
+export default List;
