1938d4d6d865d1a286f97d055943f830b595d9f4
UbikK <cohen.alberic@gmail.com>
2023-02-09 16:03:21 +0100

    domain deno

--- packages/domain/deno/mod.ts
+++ packages/domain/deno/mod.ts
@@ -2,13 +2,13 @@ import Address, { AddressDto } from "../src/address/address.model.ts";
 import PointOfInterest, { PointOfInterestDto } from "../src/pointOfInterest/poi.model.ts";
 import User, { UserDto } from "../src/user/user.model.ts";
 
-export type { IAddressAdapter } from '../src/address/index.ts';
-export type { IInformationService, IPointOfInterestAdapter } from '../src/pointOfInterest/index.ts';
+export type { IAddressAdapter } from '../src/address/addressAdapter.interface.ts';
+export type { IInformationService } from '../src/pointOfInterest/informationService.interface.ts';
+export type { IPointOfInterestAdapter } from '../src/pointOfInterest/poiAdapter.interface.ts';
 export type { IRepository } from '../src/Repository.interface.ts';
 export type { IUseCase } from '../src/UseCase.ts';
-export type { IUserAdapter } from '../src/user/index.ts';
-export type { UserDto };
-export type { PointOfInterestDto };
-export type { AddressDto };
+export type { IUserAdapter } from '../src/user/userAdapter.interface.ts';
+export type { UserDto, PointOfInterestDto, AddressDto };
 export { User, Address, PointOfInterest };
+
 
